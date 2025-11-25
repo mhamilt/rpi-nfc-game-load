dae5bf3ae8a09234bb04b3014a8e7b587e774c5c
Matthew Hamilton <matthew.hamilton2@unibo.it>
2025-11-25 13:23:18 +0000

    and NFC code

---
+++ examples/load_game.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
+#include "pn532.h"
+#include "pn532_rpi.h"
+ 
+int main(void) {
+    system("date +%A");
+    system("gcc --version");
+}
