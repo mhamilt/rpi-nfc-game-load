69a288a99193c31f39eb09872b6247ad94a99478
Matthew Hamilton <matthew.hamilton2@unibo.it>
2025-11-26 16:41:10 +0000

    change name

--- src/nfc_launch.c
+++ src/nfc_launch.c
@@ -23,7 +23,7 @@ char system_command[sizeof(path) + 100];
 const char* system_command_format = "/opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ %s /home/pi/RetroPie/roms/%s/%s";
 
 Game gamelist[] = {
-    {.card_id =  {0x80, 0xc4, 0x93, 0x97}, .console = "nes", .filename = "Gatsby.nes"},
+    {.card_id =  {0x80, 0xc4, 0x93, 0x97}, .console = "nes", .filename = "gatsby.nes"},
     {.card_id =  {0X4B, 0xEB, 0x08, 0x25}, .console = "nes", .filename = "turtles.nes"}
 };
 
