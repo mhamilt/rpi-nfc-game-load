2804841dd035a2ddb5de4a9d10d59908a478c415
Matthew Hamilton <matthew.hamilton2@unibo.it>
2025-11-26 12:55:49 +0000

    Create sdl_text.c

---
+++ src/sdl_text.c
@@ -0,0 +1,60 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    TTF_Init();
+
+    SDL_Window* window = SDL_CreateWindow("SDL Text Example",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          800, 600, 0);
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    // Load font (adjust path and size)
+    TTF_Font* font = TTF_OpenFont("DejaVuSans.ttf", 48);
+    if (!font) {
+        SDL_Log("Failed to load font: %s", TTF_GetError());
+        return 1;
+    }
+
+    // Set text color
+    SDL_Color color = { 255, 255, 255, 255 }; // white
+
+    // Render text surface
+    SDL_Surface* surface = TTF_RenderText_Solid(font, "Hello SDL!", color);
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+
+    int running = 1;
+    SDL_Event e;
+
+    SDL_Rect dest;
+    dest.x = 100; dest.y = 100;
+    SDL_QueryTexture(texture, NULL, NULL, &dest.w, &dest.h);
+
+    while (running) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) running = 0;
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderCopy(renderer, texture, NULL, &dest);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyTexture(texture);
+    TTF_CloseFont(font);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    TTF_Quit();
+    SDL_Quit();
+
+    return 0;
+}
