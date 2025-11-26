575e19d382f0f1729149d1991639e9f3f3f6a557
Matthew Hamilton <matthew.hamilton2@unibo.it>
2025-11-26 11:36:35 +0000

    Update nfc_launch.c

--- examples/nfc_launch.c
+++ examples/nfc_launch.c
@@ -1,14 +1,32 @@
-#include <stdio.h>
+//
+// Basic Launch a game using NFC
+//
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 #include <time.h>
 #include "pn532.h"
 #include "pn532_rpi.h"
 
+const uint8_t numGames = 2;
+const char* path = "/opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ nes /home/pi/RetroPie/roms/nes/";
+char system_command[sizeof(path) + 100];
+
+uint8_t target_uids[numGames][MIFARE_UID_MAX_LENGTH] = {
+    {0x80, 0xc4, 0x93, 0x97},
+    {0X4B, 0xEB, 0x08, 0x25}
+};
+const char* gamelist[numGames] = {
+    "turtle.nes",
+    "Gatsby.nes",
+};
+
+
 int main(int argc, char** argv) {
     uint8_t buff[255];
     uint8_t uid[MIFARE_UID_MAX_LENGTH];
-    uint8_t target_uid[MIFARE_UID_MAX_LENGTH] = {0X4B,0xEB,0x08,0x25};
+    
     int32_t uid_len = 0;
     PN532 pn532;
     
@@ -25,13 +43,31 @@ int main(int argc, char** argv) {
     {
         // Check if a card is available to read
         uid_len = PN532_ReadPassiveTarget(&pn532, uid, PN532_MIFARE_ISO14443A, 1000);
-        if (uid_len == PN532_STATUS_ERROR) {
+        if (uid_len == PN532_STATUS_ERROR) { // Waiting
             fflush(stdout);
-        } else {                        	        
-            if(*((uint32_t*)target_uid) == *((uint32_t*)uid))
-                system("/opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ nes /home/pi/RetroPie/roms/nes/turtle.nes");
+        } else {
+            
+            bool gameFound = false;
+            int gameNum;
+            for (int i = 0; i < numGames; i++)            
+            {
+                if(*((uint32_t*)target_uid[i]) == *((uint32_t*)uid))
+                {
+                    gameNum = i
+                    break;
+                }
+            }
+
+            if(gameFound)
+            {
+                strcpy(system_command, path);
+                strcat(system_command, gamelist[gameNum]);
+                printf("%s\n", system_command);
+            }
             else
+            {
                 printf("Card Not Recognised\r\n\r\n");
+            }
             break;
         }
     }
