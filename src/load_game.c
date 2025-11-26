2e6cab35501500116951c0f03d8db817e69e377f
Matthew Hamilton <matthew.hamilton2@unibo.it>
2025-11-26 16:41:04 +0000

    try execl

--- src/load_game.c
+++ src/load_game.c
@@ -1,14 +1,31 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <time.h>
 #include "pn532.h"
 #include "pn532_rpi.h"
- 
-int main(void) {
-    system("date +%A");
-    system("gcc --version");
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-    system("/opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ nes /home/pi/RetroPie/roms/nes/turtle.nes");
-    
+int main(void) {
+  system("date +%A");
+  system("gcc --version");
+
+  pid_t pid = fork();
+  if (pid == 0) {
+    // Child process: replace with runcommand
+    execl("/opt/retropie/supplementary/runcommand/runcommand.sh",
+          "runcommand.sh", "0", "_SYS_", "nes",
+          "/home/pi/RetroPie/roms/nes/game.nes", (char *)NULL);
+
+    // If execl returns, something failed:
+    perror("execl failed");
+    exit(1);
+  }
+
+  // Parent process: wait until emulator finishes
+  int status;
+  waitpid(pid, &status, 0);
+
+  //   system("/opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ nes
+  //   "
+  //          "/home/pi/RetroPie/roms/nes/turtle.nes");
 }
